c49d07cdd96d449cf6be7e9ba5f958672d62f704
g1gzer <rhinorishe@gmail.com>
2020-11-02 21:21:24 +0300

    ''

---
+++ src/Components/Header.css
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <div className='header__logo'>
+        <img src='https://www.flaticon.com/svg/static/icons/svg/842/842184.svg'></img>
+        </div>
+    </header>
+}
+
+export default Header;
