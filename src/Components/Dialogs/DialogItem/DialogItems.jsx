3d227ff6caa8cb38d458e31a1d9f27729fc77e19
g1gzer <rhinorishe@gmail.com>
2020-11-16 13:06:39 +0300

    Added state, optimized code

---
+++ src/Components/Dialogs/DialogItem/DialogItems.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './../Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.item}>
+            <NavLink to={'/Dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+
+export default DialogItem;
---
+++ src/Redux/state.js
@@ -0,0 +1,20 @@
+let dialogsData = [
+    {id: 1, name: 'Andryusha'},
+    {id: 2, name: 'Sergey'},
+    {id: 3, name: 'Tatiana'},
+    {id: 4, name: 'Kolya'},
+    {id: 5, name: 'Ilya'},
+];
+
+let messagesData = [
+    {id: 1, text: 'Hi Dude!'},
+    {id: 2, text: 'Hi! my friend!'},
+    {id: 3, text: 'Problem?'},
+    {id: 4, text: 'Anime?'},
+    {id: 5, text: 'Thanks:)'},
+];
+
+
+let state = {
+
+};
