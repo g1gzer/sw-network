49fd6dc567243aeb035addc94d3f713e557b30e9
g1gzer <rhinorishe@gmail.com>
2020-11-29 13:13:01 +0300

    Refactor state managment

---
+++ src/Components/Dialogs/DialogsInput/DialogsInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from "../Dialogs.module.css";
+
+
+
+const DialogInput = (props) => {
+    return (
+    <div className={s.input}>
+        <textarea></textarea>
+        <button></button>
+    </div>
+    )
+
+}
--- src/Components/Dialogs/DialogsItems/DialogsItems.jsx
+++ src/Components/Dialogs/DialogsItems/DialogsItems.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import s from './../Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
-const DialogItem = (props) => {
+const DialogsItem = (props) => {
     return (
         <div className={s.item}>
+            <img className={s.userImage} src={props.userImage}></img>
             <NavLink to={'/Dialogs/' + props.id}>{props.name}</NavLink>
         </div>
     )
 }
 
 
-export default DialogItem;
+export default DialogsItem;
